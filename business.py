b6552a5b2a2048397c24bc772f1165dc287cadf2
subramaniannk <subramanian.nk@live.com>
2021-04-21 17:05:52 +0530

    Separating business from rest

--- business.py
+++ business.py
@@ -1,7 +1,56 @@
+from flask import Flask, jsonify, request
+import jwt
+from persistence_gateway import PersistenceGateway
+users = {}
+
+storage = PersistenceGateway()
+
 class Business:
     def __init__(self):
-        self.myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        self.mydb = self.myclient["mydatabase"]
+        self.a = 5
 
-    def register(self, request):
-        pass
+    def register(self, r):
+        payload = r.get_json()
+        first_name = payload['first_name']
+        last_name = payload['last_name']
+        email = payload['email']
+        password = payload['password']
+        users[email] = password
+        print(users)
+
+    def get(self, r):
+        authorization_value = r.headers.get('Authorization')
+        authorization_value = authorization_value[7:]
+        key = "secret"
+        credentials = jwt.decode(authorization_value, key, verify=True, algorithm="HS256")
+        email = credentials["email"]
+        temp = storage.get(email, 55)
+        return jsonify(temp), 202
+
+    def update(self, r):
+        authorization_value = r.headers.get('Authorization')
+        authorization_value = authorization_value[7:]
+        key = "secret"
+        credentials = jwt.decode(authorization_value, key, verify=True, algorithm="HS256")# try
+        email = credentials["email"]
+        if email in users.keys():
+            mydict = {"name": "John", "address": "Highway 37", "_id": 55}
+            storage.add(email, mydict)
+            print("donebngdhnjtgjhnfgjmfgjfgjfgjrtj")
+            return credentials
+        else:
+            print("I am not allowed")
+            return {'error': "access denied"}, 401
+
+    def login(self, r):
+        payload = r.get_json()
+        email = payload['email']
+        password = payload['password']
+        if users[email] == password:
+            key = "secret"
+            token = jwt.encode({"email": email, "password": password}, key, algorithm="HS256").decode("UTF-8")
+            return jsonify({"token": token}), 202
+        else:
+            return {'error': "access denied"}, 401
+
+
--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from flask import Flask, jsonify, request
 import jwt
 import pymongo
 from persistence_gateway import PersistenceGateway
+from business import Business
 
 app = Flask(__name__)
 storage = PersistenceGateway()
 
 users = {}
-
+t = Business()
 
 @app.route("/register")
 def register_function():
+    t.register(request)
     payload = request.get_json()
     first_name = payload['first_name']
     last_name = payload['last_name']
@@ -26,45 +28,40 @@ def register_function():
 
 @app.route("/login")
 def login_function():
-    payload = request.get_json()
-    email = payload['email']
-    password = payload['password']
-    if users[email] != password:
-        return {'error': "access denied"}, 401
-    else:
-        key = "secret"
-        token = jwt.encode({"email": email}, key, algorithm="HS256").decode("UTF-8")
-        return jsonify({"token": token}), 202
+    return t.login(request)
 
 
 
 @app.route("/checking")
 def get_function():
-    authorization_value = request.headers.get('Authorization')
-    authorization_value = authorization_value[7:]
-    key = "secret"
-    credentials = jwt.decode(authorization_value, key, verify=True, algorithm="HS256")
-    email = credentials["email"]
-    temp = storage.get(email, 28)
-    return jsonify(temp), 202
+    return t.get(request)
+    #authorization_value = request.headers.get('Authorization')
+    #authorization_value = authorization_value[7:]
+    #key = "secret"
+    #credentials = jwt.decode(authorization_value, key, verify=True, algorithm="HS256")
+    #email = credentials["email"]
+    #temp = storage.get(email, 28)
+    #return jsonify(temp), 202
 
 
 
 @app.route("/template")
 def update_function():
-    authorization_value = request.headers.get('Authorization')
-    authorization_value = authorization_value[7:]
-    key = "secret"
-    credentials = jwt.decode(authorization_value, key, verify=True, algorithm="HS256")# try
-    email = credentials["email"]
-    if email in users.keys():
-        mydict = {"name": "John", "address": "Highway 37", "_id": 77}
-        storage.add(email, mydict)
-        print("donebngdhnjtgjhnfgjmfgjfgjfgjrtj")
-        return credentials
-    else:
-        print("I am not allowed")
-        return {'error': "access denied"}, 401
+    return t.update(request)
+    #authorization_value = request.headers.get('Authorization')
+    #authorization_value = authorization_value[7:]
+    #key = "secret"
+    #credentials = jwt.decode(authorization_value, key, verify=True, algorithm="HS256")# try
+    #email = credentials["email"]
+    #if email in users.keys():
+        #mydict = {"name": "John", "address": "Highway 37", "_id": 77}
+        #storage.add(email, mydict)
+        #print("donebngdhnjtgjhnfgjmfgjfgjfgjrtj")
+        #return credentials
+    #else:
+        #print("I am not allowed")
+        #return {'error': "access denied"}, 401
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
